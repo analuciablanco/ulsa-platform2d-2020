65e88adbd3c96b9e70cacc9dff337e943ce2debf
Ana Lucia Blanco <analuciablanco@outlook.com>
2020-02-05 07:54:35 -0700

    enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] float moveSpeed;
+    [SerializeField] float moveSpeed = 3f;
+
+    [SerializeField] float delay = 2f;
+
+    float timer;
+
+    [SerializeField] Vector2 dir;
 
     private void Update() 
     {
-        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        transform.Translate(dir * moveSpeed * Time.deltaTime);
+
+        timer += Time.deltaTime;
+
+        if(timer >= delay)
+        {
+            timer = 0;
+            dir = dir == Vector2.right ? Vector2.left : Vector2.right;
+        }
     }
 }
